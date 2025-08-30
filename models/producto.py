b64219e49f0f553313b3da13bc8a3fadb058ed26
juan miguel ceron ocampo <juanmiguelceronocampo@gmail.com>
2025-08-30 10:05:29 -0500

    refactor(model): actualizar clase Producto en models

--- models/producto.py
+++ models/producto.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Producto(BaseModel):
-    id: int
+    id: Optional[int] = None   # el id puede venir vacío al crearlo, lo asignamos después
     nombre: str
-    cantidad: int
+    descripcion: Optional[str] = None
     precio: float
+    stock: int
